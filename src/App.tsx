d591959b8f810975feaac5da2e104ade474cb714
Travis Barnette <barnette.travis31@gmail.com>
2025-11-11 16:48:20 -0500

    rebuild after refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 function App() {
   //@ts-ignore
-  const version = import.meta.VITE_PR_VERSION || 'dev';
+  const version = import.meta.env.VITE_PR_VERSION || 'dev';
+  console.log(import.meta.env)
   return (
-    <main className="flex-1 flex justify-center py-4">
+    <main className="flex-1 flex justify-center py-4 flex-col">
       <h1 className="text-3xl text-red-600">Hello from my preview</h1>
-      <p>Current time {new Date().toLocaleDateString()}</p>
+      <p className="text-gray-50">Current time {new Date().toLocaleDateString()}</p>
       <p>Build version: {version}</p>
     </main>
   )
