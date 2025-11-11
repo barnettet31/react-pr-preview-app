c5e94e48dcec3bac51f1ca16e121d0e5fe1486aa
Travis Barnette <barnette.travis31@gmail.com>
2025-11-11 15:16:11 -0500

    add ci pipeline

---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM node:20-alpine AS build
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci 
+COPY . . 
+ARG VITE_PR_NUMBER
+ENV VITE_PR_NUMBER=$VITE_PR_NUMBER
+RUN npm run build
+FROM nginx:alpine
+COPY --from=build /app/dist /user/share/nginx/html
+EXPOSE 80
+CMD ['nginx', '-g', 'daemon off;']
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   //@ts-ignore
-  const version = process.env.REACT_APP_VERSION || 'dev';
+  const version = import.meta.VITE_PR_VERSION || 'dev';
   return (
     <main className="flex-1 flex justify-center py-4">
       <h1 className="text-3xl text-red-600">Hello from my preview</h1>
