aefc73c23c51cb09559cb53d056b0c64ca688ac7
Travis Barnette <barnette.travis31@gmail.com>
2025-11-11 14:36:36 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  //@ts-ignore
+  const version = process.env.REACT_APP_VERSION || 'dev';
+  return (
+    <main className="flex-1 flex justify-center py-4">
+      <h1 className="text-3xl text-red-600">Hello from my preview</h1>
+      <p>Current time {new Date().toLocaleDateString()}</p>
+      <p>Build version: {version}</p>
+    </main>
+  )
+}
+
+export default App
